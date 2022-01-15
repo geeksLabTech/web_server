e9e0875b715d787233fdb2ec3b13f59de7675765
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2022-01-14 21:56:25 -0500

    Create_test_for_server_implementation

---
+++ test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "server.h"
+
+void launch(struct Server *server){
+    char buffer[30000];
+    char *hello = "HTTP/1:1 200 OK\nGMT\nServer: Apache/2.2.14 (Win32)\nLast-Modified: Wed, 22 Jul 2009 19:15:56 GMT\nContent-Type: text/html\nConnection: Closed\n\n<html><body><ch1>Homo Deus</h1></body></html>";
+    int address_length = sizeof(server->address);
+    int new_socket;
+    while (1)
+    {
+        printf("===== WAITING FOR CONNECTION =====\n");
+        new_socket = accept(server->socket, (struct sockaddr *)&server->address, (socklen_t *)&address_length);
+        read(new_socket, buffer, 30000);
+        printf("%s\n", buffer);
+        write(new_socket, hello, strlen(hello));
+        close(new_socket);
+        
+    }
+    
+}
+
+int main() {
+    struct Server server = server_constructor(AF_INET, SOCK_STREAM, 0, INADDR_ANY, 443, 10, launch);
+    server.launch(&server);
+}
